5cbdc83f583dd5df5d972baf8777ba1d97c089e2
Anas Ryadi <anasryadi@Anass-MBP.hsd1.ca.comcast.net>
2020-12-17 23:41:16 -0800

    done for the day

--- index.js
+++ index.js
@@ -75,8 +75,28 @@ class Airplane {
   */
   
  class Car {
-    
-  }
+   constructor(model, milesPerGallon){
+     this.model = model
+     this.milesPerGallon = milesPerGallon
+     this.tank = 0
+     this.odometer = 0
+   }
+   fill(gallons){
+     this.tank += gallons
+     }
+     drive(distance){
+       if(distance >= this.tank * this.milesPerGallon){
+        this.odometer = this.odometer + this.tank * this.milesPerGallon
+        this.tank = 0
+        return `I ran out of fuel at ${this.odometer} miles!`
+       }else{
+         this. odometer = this.odometer + distance
+         this.tank = ((this.tank * this.milesPerGallon) - distance) / this.milesPerGallon
+       }
+
+      }
+    }
+  
   
   /*
     TASK 3
